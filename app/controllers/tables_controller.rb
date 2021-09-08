979c3444a75cb8ac82ac1007705ecd87697c8a7b
Trevor Vallender <tsv@tsv.me.uk>
2021-09-08 11:53:27 +0100

    Added table editing/deletion

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,6 +1,7 @@
 class TablesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_table, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_table, only: [:show, :edit, :update, :destroy]
+  before_action :table_owner, only: [:edit, :update, :destroy]
   
   def index
     @tables = Table.all.paginate(page: params[:page])
@@ -23,6 +24,24 @@ class TablesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @table.update(table_params)
+      flash[:success] = "Table updated!"
+      redirect_to @table
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @table.destroy
+    flash[:success] = "Table deleted"
+    redirect_to tables_path
+  end
+  
   private
   def table_params
     params.require(:table).permit(:title, rows_attributes: [:id, :name, :description, :_destroy])
@@ -30,5 +49,9 @@ class TablesController < ApplicationController
 
   def set_table
     @table = Table.find(params[:id])
+  end
+
+  def table_owner
+    redirect_to root_path unless current_user = @table.user
   end
 end
