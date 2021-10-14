b618fc265b56c2d9946632b8c351ea3223c08844
Trevor Vallender <tsv@tsv.me.uk>
2021-10-14 12:17:58 +0100

    Fixed following association, feed works correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,17 @@ class User < ApplicationRecord
   has_many :tables, dependent: :destroy
   has_many :lists, dependent: :destroy
 
-  has_many :followers, class_name: "User", foreign_key: "following_id"
-  has_many :following, class_name: "User", foreign_key: "follower_id"
+  has_many :f_followers, class_name: 'Follow', foreign_key: "following_id"
+  has_many :f_following, class_name: 'Follow', foreign_key: "follower_id"
+
+  has_many :followers, through: 'f_followers', source: 'follower'
+  has_many :following, through: 'f_following', source: 'following'
   
   def to_param
     username
+  end
+
+  def activity_feed
+    PublicActivity::Activity.where(owner: following)
   end
 end
